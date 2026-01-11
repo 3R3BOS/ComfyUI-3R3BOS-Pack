a09458d37d7aa0bd73472f318a04f26430ad84c4
3R3BOS <cyb3r.s1lv3rtaurus@gmail.com>
2026-01-11 13:11:33 +0900

    Update __init__.py
    Update

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-# Author: 3R3BOS
-
-from .image_comparer_slider import RefinerCompareNode
+# Developed by 3R3BOS
+from .nodes.visual_gatekeeper import VisualGatekeeper
+from .nodes.image_comparer_slider import ImageComparerSlider
 
 NODE_CLASS_MAPPINGS = {
-    "RefinerCompareNode": RefinerCompareNode
+    "Visual Gatekeeper (3R3BOS)": VisualGatekeeper,
+    "Image Comparer (3R3BOS)": ImageComparerSlider
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "RefinerCompareNode": "Image Comparer Slider (3R3BOS)"
+    "Visual Gatekeeper (3R3BOS)": "Visual Gatekeeper",
+    "Image Comparer (3R3BOS)": "Image Comparer Slider"
 }
 
 WEB_DIRECTORY = "./js"
