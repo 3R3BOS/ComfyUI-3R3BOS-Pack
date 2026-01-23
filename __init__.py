67ffef530b1a98dcd6e6e34c59ea184198309eba
3R3BOS <cyb3r.s1lv3rtaurus@gmail.com>
2026-01-23 20:17:07 +0900

    Add Aspect Ratio Master to node mappings

--- __init__.py
+++ __init__.py
@@ -2,19 +2,22 @@
 
 from .nodes.image_comparer_slider import ImageComparerSlider
 from .nodes.batch_selector import BatchSelector
+from .nodes.aspect_ratio_master import AspectRatioMaster
 
 NODE_CLASS_MAPPINGS = {
 
     "Image Comparer (3R3BOS)": ImageComparerSlider,
-    "Batch Selector (3R3BOS)": BatchSelector
+    "Batch Selector (3R3BOS)": BatchSelector,
+    "Aspect Ratio Master (3R3BOS)": AspectRatioMaster
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
 
     "Image Comparer (3R3BOS)": "Image Comparer Slider",
-    "Batch Selector (3R3BOS)": "Batch Selector"
+    "Batch Selector (3R3BOS)": "Batch Selector",
+    "Aspect Ratio Master (3R3BOS)": "Aspect Ratio Master"
 }
 
-WEB_DIRECTORY = "./js"
 
+WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
